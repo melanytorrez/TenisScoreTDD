448f0e7da376cf62776948a9962c353b8e8f45ec
melanytorrez <torrezmelany13@gmail.com>
2025-03-06 09:39:02 -0400

    feat: calcular marcador 1-0 y 2-0

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,5 +1,12 @@
-function puntuacion(){
-    return "love - love"
+function obtenerScore(jugador1, jugador2) {
+  if (jugador1 === 1 && jugador2 === 0) {
+    return "15 - 0"
+  }
+  if (jugador1 === 2 && jugador2 === 0) {
+    return "30 - 0"
+  }
+  return "love - love"
 }
 
-export default puntuacion;
+
+export default obtenerScore;
--- src/puntaje.spec.js
+++ src/puntaje.spec.js
@@ -1,7 +1,15 @@
-import puntuacion from "./puntaje.js";
+import obtenerScore from "./puntaje.js";
 
 describe("Marcador de puntaje de tenis", () => {
   it("Deberia devolver el puntaje de ambos jugadores", () => {
-    expect(puntuacion(0,0)).toEqual("love - love");
+    expect(obtenerScore(0,0)).toEqual("love - love");
+  });
+
+  it("Deberia devolver el marador 1-0", () => {
+    expect(obtenerScore(1,0)).toEqual("15 - 0");
+  });
+
+  it("Deberia devolver el marador 2-0", () => {
+    expect(obtenerScore(2,0)).toEqual("30 - 0");
   });
 });
